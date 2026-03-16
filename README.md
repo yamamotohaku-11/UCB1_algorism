82df0de96d6555e125ee5f5d7f9f5490cab3780e
yamamotohaku-11 <yamamoto.haku.t6@elms.hokudai.ac.jp>
2026-03-16 11:29:25 +0900

    aaa

--- README.md
+++
@@ -1,3 +0,0 @@
-
-# UCB1_algorism
->>>>>>> aed052df8c280c17f43cdf848a04f858e94359e9
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from UCB1_algorism.UCB1algorism import UCB1algorism
-import numpy 
+import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from UCB1_algorism.beta_distribution import distribution
 def main():
+    D = distribution(4,True)
+    print(D)
     y = [0 for i in range(50)]
     Amax = 0
     summax = 0
@@ -10,14 +13,16 @@ def main():
         n = i * 0.1
         sum = 0
         for j in range(500):
-            sum += UCB1algorism(4,10000,n)
+            sum += UCB1algorism(4,10000,n,D)
         y[i-1] = sum / 500
         if summax < sum:
             summax = sum
-            Amax = i    
+            Amax = n
     x = [i for i in range(1,51)]
-    
+    variance = np.var(D)
+    roundedD = [round(i,3) for i in D]
     plt.plot(x,y)
+    plt.figtext(0.5, 0.01, f"distribution:{roundedD}", ha="center")
     plt.savefig("graph.png")
     plt.show()
     print(Amax)
